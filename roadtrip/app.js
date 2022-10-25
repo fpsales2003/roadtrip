0156e2dda9fc714ed2b57b5273aa676d7cfc982f
fpsales2003 <95240671+fpsales2003@users.noreply.github.com>
2022-10-24 21:48:46 -0400

    text

---
+++ roadtrip/app.js
@@ -0,0 +1,8 @@
+$('.rttext').css({
+    opacity: function() {
+        var elementHeight = $(this).height(),
+            opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
+
+return opacity;
+}
+});
